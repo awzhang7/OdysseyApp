717ce119d940465969d55ede3ea938063a1500e0
awzhang7 <awzhang@umich.edu>
2025-02-13 01:21:47 -0500

    Initial Commit

---
+++ Odyssey/OdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyApp.swift
+//  Odyssey
+//
+//  Created by Andrew Zhang on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct OdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OdysseyTests/OdysseyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyTests.swift
+//  OdysseyTests
+//
+//  Created by Andrew Zhang on 2/13/25.
+//
+
+import Testing
+@testable import Odyssey
+
+struct OdysseyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
